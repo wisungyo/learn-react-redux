202c555d1eecaedc3bbe3222e2766c568935a13f
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-08-30 19:29:03 +0700

    adding documentation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
+// action we create
 const BUY_CAKE = 'BUY_CAKE'
 
+// action to reduce numOfCakes by 1
 function buyCake() {
     return {
         type: BUY_CAKE,
@@ -28,9 +30,14 @@ const reducer = (state = initialState, action) => {
 }
 
 const store = createStore(reducer)
+
 console.log('Initial state', store.getState())
+
 const unsubcribe = store.subscribe(() => console.log('Updated state', store.getState()))
+
+// update the state using action 'BUY_CAKE'
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+
 unsubcribe()
