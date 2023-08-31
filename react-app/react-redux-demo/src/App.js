86c31b2d386a6380655047e617876c2ce15de7f3
Wisnu Agung Prasetyo <wisungyo@gmail.com>
2023-08-31 09:08:55 +0700

    adding react app

---
+++ react-app/react-redux-demo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
